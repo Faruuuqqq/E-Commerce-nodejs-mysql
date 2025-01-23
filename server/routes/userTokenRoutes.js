71c6f0f6ce78ecf9d240a1e9fae0499277c6f31d
Faruuuqqq <faruqmahdison@gmail.com>
2025-01-23 18:37:42 +0700

    Add route files for cart, order, product, user, and token management

---
+++ server/routes/userTokenRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const tokenController = require("../controllers/tokenController");
+
+// Route to get refreshToken
+router.post("/refreshToken", tokenController.getRefreshToken);
+
+module.exports = router;
