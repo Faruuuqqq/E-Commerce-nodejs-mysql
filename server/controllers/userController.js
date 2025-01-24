0ad1648d820242bc6a2be5f30a08d8eea6eb4b10
Faruuuqqq <faruqmahdison@gmail.com>
2025-01-24 23:09:47 +0700

    Add change password functionality to user controller with error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,11 +4,10 @@ const userModel = require('../models/userModel');
 exports.register = async (req, res) => {
   const { email, password, isAdmin, fname, lname } = req.body;
 
-  // Validate input
+// Validate input
   if (!email || !password || !fname || !lname) {
     return res.status(400).json({ error: "Email, password, first name, and last name are required" });
   }
-
   try {
     const result = await userModel.register(email, password, isAdmin, fname, lname);
     console.log('User registered successfully');
@@ -38,5 +37,19 @@ exports.login = async (req, res) => {
     } else {
       res.status(500).json({ error: "Internal Server Error" });
     }
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  const { email, oldPassword, newPassword } = req.body;
+
+  try {
+      const result = await userModel.changePassword(email, oldPassword, newPassword);
+      
+      console.log("Password changed successfully");
+      res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+      console.error("Error changing password:", error.message);
+      res.status(400).json({ error: error.message });
   }
 };
