e6d2872144fe00059a0bc10281b7be6b452f6bdc
Faruuuqqq <faruqmahdison@gmail.com>
2025-02-09 23:13:32 +0700

    Refactor server routes and update middleware configuration

--- server/app.js
+++ server/app.js
@@ -1,27 +1,35 @@
 // app.js
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-const userToken = require("./routes/userTokenRoute")
+//const userToken = require("./routes/userTokenRoute")
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.json()); // parse application/json
+// app.use(express.static('public')); // to serve static file like CSS
+app.use(express.static(path.join(__dirname, "public")));
 // Connect to the database
 
 // Mount routes
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/token", userToken)
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/cart", cartRoutes);
+//app.use("/api/token", userToken)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
