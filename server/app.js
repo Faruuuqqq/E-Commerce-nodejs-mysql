77523b372f7b2501948d5a23f3749be36219f98e
Faruuuqqq <faruqmahdison@gmail.com>
2025-02-11 23:48:57 +0700

    add some middleware and home routes

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const path = require('path');
+const cookieParser = require("cookie-parser");
 
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 //const userToken = require("./routes/userTokenRoute")
+const homeRoutes = require("./routes/homeRoutes");
 
 const app = express();
 
@@ -17,10 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true })); // for form data
-app.use(express.json()); // parse application/json
-// app.use(express.static('public')); // to serve static file like CSS
+app.use(express.json()); // parse application
 app.use(express.static(path.join(__dirname, "public")));
-// Connect to the database
+app.use(cookieParser());
 
 // Mount routes
 app.use("/users", userRoutes);
@@ -28,9 +28,10 @@ app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 app.use("/cart", cartRoutes);
 //app.use("/api/token", userToken)
+app.use("/", homeRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
