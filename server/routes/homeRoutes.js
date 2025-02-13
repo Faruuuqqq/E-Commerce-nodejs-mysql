4915df958ff26bcdb3b2cf5dfff0106b53eec1b4
Faruuuqqq <faruqmahdison@gmail.com>
2025-02-13 23:51:30 +0700

    Refactor home routes to utilize authentication middleware and include product data in the homepage render

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -2,30 +2,12 @@ const express = require('express');
 const router = express.Router();
 const productModel = require("../models/productModel");
 const userModel = require("../models/userModel");
-const jwt = require('jsonwebtoken');
-
-// middleware to get user from JWT token
-const authenticateUser = async (req, res, next) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-      return res.status(401).json({ error: "Authentication error: No token provided" });
-  }
-
-  try {
-      const decoded = jwt.verify(token, "secret-key");
-      req.user = decoded;
-      next();
-  } catch (error) {
-      return res.status(401).json({ error: "Invalid token" });
-  }
-};
-
+const authenticateUser = require("../middleware/authenticateUser");
 
 router.get("/home", authenticateUser, async (req, res) => {
   try {
-    // const products = await productModel.getAllProducts();
-    res.render("home", { user: req.user });
+    const products = await productModel.getAllProducts();
+    res.render("home", { user: req.user, products });
   } catch (error) {
     console.error("Error rendering homepage:", error);
     res.status(500).send("internal server error");
