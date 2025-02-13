ab87dd378faa138501eabdab5634449d69db473f
Faruuuqqq <faruqmahdison@gmail.com>
2025-02-13 23:50:10 +0700

    Refactor token utility to use class structure and improve token generation and verification methods

--- server/utils/token.js
+++ server/utils/token.js
@@ -1,43 +1,42 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config(); // Load environment variables from .env file
+const express = require("express");
+require('dotenv').config();
 
-// Secret key for JWT
-const JWT_SECRET_KEY_ACCESS_TOKEN = process.env.JWT_SECRET_KEY_ACCESS_TOKEN; // Replace this with your secret key
-const JWT_SECRET_KEY_REFRESH_TOKEN = process.env.JWT_SECRET_KEY_REFRESH_TOKEN;
+class TokenService {
+  static JWT_SECRET_KEY_ACCESS_TOKEN = process.env.JWT_SECRET_KEY_ACCESS_TOKEN;
+  static JWT_SECRET_KEY_REFRESH_TOKEN = process.env.JWT_SECRET_KEY_REFRESH_TOKEN;
 
-// Generate JWT token
-exports.generateAccessAndRefreshToken = (payload) => {
-    let token = jwt.sign(payload, JWT_SECRET_KEY_ACCESS_TOKEN, { expiresIn: '3m' }); // Token expires in 1 hour
-    let refreshToken = jwt.sign(payload, JWT_SECRET_KEY_REFRESH_TOKEN, { expiresIn: '3m' });
-    return {token, refreshToken}
-};
+// Generate JWT access & refresh token
+  static generateTokens(payload) {
+    const accessToken = jwt.sign(payload, this.JWT_SECRET_KEY_ACCESS_TOKEN, { expiresIn: '15m' }); // Access token: 15 minutes
+    const refreshToken = jwt.sign(payload, this.JWT_SECRET_KEY_REFRESH_TOKEN, { expiresIn: '7d' }); // Refresh token: 7 days
 
+    return { accessToken, refreshToken };
+  };
 
 // Verify JWT token
-exports.verifyToken = (token) => {
+  static async verifyAccessToken(token) {
     return new Promise((resolve, reject) => {
         jwt.verify(token, JWT_SECRET_KEY_ACCESS_TOKEN, (err, decoded) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(decoded);
-            }
+            if (err) reject(err);
+            else resolve(decoded);
         });
     });
-};
+  };
 
 // Refresh JWT token
-exports.refreshToken = (refreshToken) => {
+  static async refreshAccessToken(refreshToken) {
     return new Promise((resolve, reject) => {
         jwt.verify(refreshToken, JWT_SECRET_KEY_REFRESH_TOKEN, (err, decoded) => {
-            if (err) {
-                reject(err);
-            } else {
+            if (err) reject(err);
+            else {
                 // Generate new access and refresh tokens
                 const { userId, isAdmin } = decoded;
-                const newTokens = this.generateAccessAndRefreshToken({ userId, isAdmin });
-                resolve(newTokens);
+                resolve(this.generateTokens({ userId, isAdmin }));
             }
         });
     });
 };
+}
+
+module.exports = TokenService;
